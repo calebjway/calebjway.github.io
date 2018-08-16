6e4d286cda20c5a41dfa1184d663d2526003d51a
calebjway <39162283+cway-OHS-Harris@users.noreply.github.com>
2018-08-16 10:07:28 -0400

    PdfViewer

---
+++ Pages/PageScripts/PdfViewer.js
@@ -0,0 +1,10 @@
+function displayPDF(pdfName){
+  document.getElementById("pdfSelector").style.visibility ="hidden";
+  document.getElementById("pdfContainer").style.visibility ="visible";
+  document.getElementById("pdfContainer").innerHTML="<object data=\""+pdfName+"\" type=\"application/pdf\" width=\"100%\" height=\"95%\">\
+    <embed src=\""+pdfName+"\" width=\"100%\" height=\"95%\" />\
+      <p>This browser does not support PDFs. Please download the PDF to view it:\
+      <a href=\""+pdfName+"\">Download PDF</a>.</p>\
+    </embed>\
+  </object>";
+}
