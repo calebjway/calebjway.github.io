19963904aef719cad6009f8b1360ba608c38b777
calebjway <39162283+cway-OHS-Harris@users.noreply.github.com>
2018-08-16 10:14:33 -0400

    Updating style, bookmarks

--- Pages/PageScripts/PdfViewer.js
+++ Pages/PageScripts/PdfViewer.js
@@ -1,7 +1,7 @@
 function displayPDF(pdfName){
   document.getElementById("pdfSelector").style.visibility ="hidden";
   document.getElementById("pdfContainer").style.visibility ="visible";
-  document.getElementById("pdfContainer").style ="background-color: #333;";
+  document.getElementById("pdfContainer").style ="background-color: #3d3d3d;";
   document.getElementById("pdfContainer").innerHTML="<object data=\""+pdfName+"\" type=\"application/pdf\" width=\"100%\" height=\"95%\">\
     <embed src=\""+pdfName+"\" width=\"100%\" height=\"95%\" />\
       <p>This browser does not support PDFs. Please download the PDF to view it:\
