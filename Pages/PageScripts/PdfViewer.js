c9de32532b9e576007a48918bdaea73cb5d57f97
calebjway <39162283+cway-OHS-Harris@users.noreply.github.com>
2018-08-21 11:55:22 -0400

    pdf

--- Pages/PageScripts/PdfViewer.js
+++ Pages/PageScripts/PdfViewer.js
@@ -1,11 +1,13 @@
 function displayPDF(pdfName){
   document.getElementById("pdfSelector").style.visibility ="hidden";
   document.getElementById("pdfContainer").style.visibility ="visible";
-  document.getElementById("pdfContainer").style ="background-color: #3d3d3d;";
+  //document.getElementById("pdfContainer").style ="background-color: #3d3d3d;";
   document.getElementById("pdfContainer").innerHTML="<object data=\""+pdfName+"\" type=\"application/pdf\" width=\"100%\" height=\"95%\">\
     <embed src=\""+pdfName+"\" width=\"100%\" height=\"95%\" />\
       <p>This browser does not support PDFs. Please download the PDF to view it:\
       <a href=\""+pdfName+"\">Download PDF</a>.</p>\
     </embed>\
-  </object>";
+  </object>\
+  <p style=\"color:white\">Click <a href=\"..\\Pages\\PdfViewer.html\">here</a> to return to the pdf selection or <a href=\"../index.html\">here</a> to return to index</p>\
+  ";
 }
